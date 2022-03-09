869dd18bb9fe80d86b3452926e4b7bc5a9ef99d7
Brian Caffey <briancaffey2010@gmail.com>
2022-03-08 21:48:12 -0500

    replace file with jsonencode for iam policies and roles

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -36,34 +36,127 @@ resource "aws_security_group" "this" {
 # assume role
 resource "aws_iam_role" "ecs_host" {
   name               = "ecs_host_role_prod"
-  assume_role_policy = file("${path.module}/policies/ecs-role.json")
+  assume_role_policy = jsonencode({
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": [
+            "ecs.amazonaws.com",
+            "ec2.amazonaws.com"
+          ]
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "ecs_instance" {
   name   = "ecs_instance_role_policy"
-  policy = file("${path.module}/policies/ecs-instance-role-policy.json")
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ecs:*",
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "ecr:*",
+          "cloudwatch:*",
+          "s3:*",
+          "rds:*",
+          "logs:*",
+          "elasticache:*",
+          "secretsmanager:*"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
   role   = aws_iam_role.ecs_host.id
 }
 
 resource "aws_iam_role" "ecs_task" {
   name               = "ecs_task_role"
-  assume_role_policy = file("${path.module}/policies/ecs-task-role.json")
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "ecs_task" {
   name   = "ecs_task_role_policy"
-  policy = file("${path.module}/policies/ecs-task-role-policy.json")
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:*"
+        ],
+        "Resource": [
+          "*"
+        ]
+      }
+    ]
+  }
+)
   role   = aws_iam_role.ecs_task.id
 }
 
 resource "aws_iam_role" "ecs_service" {
   name               = "ecs_service_role_prod"
-  assume_role_policy = file("${path.module}/policies/ecs-role.json")
+  assume_role_policy = jsonencode({
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": [
+            "ecs.amazonaws.com",
+            "ec2.amazonaws.com"
+          ]
+        },
+        "Effect": "Allow"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "ecs_service" {
   name   = "ecs_service_role_policy"
-  policy = file("${path.module}/policies/ecs-service-role-policy.json")
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "elasticloadbalancing:Describe*",
+          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+          "ec2:Describe*",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets"
+        ],
+        "Resource": [
+          "*"
+        ]
+      }
+    ]
+  })
   role   = aws_iam_role.ecs_service.id
 }
 
