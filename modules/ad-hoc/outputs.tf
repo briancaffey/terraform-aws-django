b5aa8bcb25a77c96e92e660c7ca9b1824a1da218
briancaffey <briancaffey2010@gmail.com>
2022-06-11 07:30:48 -0400

    feat(exec): add output for ecs exec

--- modules/ad-hoc/outputs.tf
+++ modules/ad-hoc/outputs.tf
@@ -5,3 +5,17 @@ output "migrate_command" {
 output "collectstatic_command" {
   value = module.collectstatic.task_execution_command
 }
+
+output "ecs_exec_command" {
+  value = <<EOT
+TASK_ARN=$(aws ecs list-tasks \
+  --cluster ${terraform.workspace}-cluster \
+  --service-name  ${terraform.workspace}-gunicorn | jq -r '.taskArns | .[0]' \
+)
+aws ecs execute-command --cluster ${terraform.workspace}-cluster \
+    --task $TASK_ARN \
+    --container gunicorn \
+    --interactive \
+    --command "/bin/bash"
+EOT
+}
