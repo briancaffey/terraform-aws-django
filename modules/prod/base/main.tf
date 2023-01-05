aa15a279e482e5d93fc5c01c35f71127969fd3b1
briancaffey <briancaffey2010@gmail.com>
2023-01-05 17:22:06 -0500

    fix(bastion): remove unused var from prod bastion

--- modules/prod/base/main.tf
+++ modules/prod/base/main.tf
@@ -86,7 +86,6 @@ module "elasticache" {
 module "bastion" {
   source          = "../../internal/bastion"
   vpc_id          = module.vpc.vpc_id
-  alb_sg_id       = module.sg.alb_sg_id
   app_sg_id       = module.sg.app_sg_id
   private_subnets = module.vpc.private_subnets
   rds_address     = module.rds.address
