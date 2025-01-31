0940c028160e5a112dfca4225dad4b778aebfd60
Brian Caffey <briancaffey2010@gmail.com>
2025-01-31 16:47:52 +0700

    fix(sg): fix cycle in sg module

--- modules/internal/sg/main.tf
+++ modules/internal/sg/main.tf
@@ -144,6 +144,15 @@ resource "aws_security_group_rule" "app_to_vpc_endpoints" {
   source_security_group_id = aws_security_group.vpc_endpoints.id
 }
 
+resource "aws_security_group_rule" "ecs_allow_https_to_vpc_endpoints" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.app.id
+  cidr_blocks       = ["10.0.0.0/8"]
+}
+
 # vpc_endpoints <- app ingress
 resource "aws_security_group_rule" "vpc_endpoints_from_app" {
   type              = "ingress"
