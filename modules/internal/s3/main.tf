64624cdc9b78019b62c33396d5de300b5481c518
Brian Caffey <briancaffey2010@gmail.com>
2025-01-31 18:21:25 +0700

    fix(s3): add BucketOwnerEnforced to assets bucket

--- modules/internal/s3/main.tf
+++ modules/internal/s3/main.tf
@@ -2,3 +2,12 @@ resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = var.force_destroy
 }
+
+# Enforce bucket ownership (disables ACLs)
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
