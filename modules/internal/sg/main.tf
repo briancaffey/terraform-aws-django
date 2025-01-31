896b73aa62b65567d44547e1fb2c21f6da5d7d92
Brian Caffey <briancaffey2010@gmail.com>
2025-01-31 15:06:28 +0700

    feat(vpc-endpoints): add vpc endpoint for ecr

--- modules/internal/sg/main.tf
+++ modules/internal/sg/main.tf
@@ -67,6 +67,14 @@ resource "aws_security_group" "app" {
   }
 }
 
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_ids
+  security_group_ids = [aws_security_group.app.id]
+}
+
 resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_id            = var.vpc_id
   service_name      = "com.amazonaws.${var.region}.ecr.dkr"
