f179077250ea22170ead959c0142e3ba482e1bca
Brian Caffey <briancaffey2010@gmail.com>
2025-01-31 15:33:00 +0700

    fix(typo): fix input typo for s3 vpc endpoint

--- modules/internal/sg/main.tf
+++ modules/internal/sg/main.tf
@@ -107,5 +107,5 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = var.vpc_id
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.private.id]
+  route_table_ids   = var.route_table_ids
 }
