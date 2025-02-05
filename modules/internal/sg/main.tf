0a33fcf8fbd708b5eb0d72da0c03815b58a49a76
Brian Caffey <briancaffey2010@gmail.com>
2025-02-05 15:38:46 +0700

    fix(lint): format tf code

--- modules/internal/sg/main.tf
+++ modules/internal/sg/main.tf
@@ -39,9 +39,9 @@ resource "aws_security_group" "app" {
 
 # Allow all traffic from ALB security group
 resource "aws_vpc_security_group_ingress_rule" "alb_ingress" {
-  ip_protocol                 = "-1"
+  ip_protocol                  = "-1"
   referenced_security_group_id = aws_security_group.alb.id
-  security_group_id        = aws_security_group.app.id
+  security_group_id            = aws_security_group.app.id
 }
 
 # Allow self-referencing traffic
@@ -54,8 +54,8 @@ resource "aws_vpc_security_group_ingress_rule" "self_ingress" {
 
 # Allow all outbound traffic
 resource "aws_vpc_security_group_egress_rule" "app_egress" {
-  ip_protocol = "-1"
-  cidr_ipv4   = "10.0.0.0/0"
+  ip_protocol       = "-1"
+  cidr_ipv4         = "10.0.0.0/0"
   security_group_id = aws_security_group.app.id
 }
 
