2550f67d566b2e11867e3125ebcd9975be866248
Brian Caffey <briancaffey2010@gmail.com>
2025-02-05 16:16:17 +0700

    fix(typo): tcp not tpc

--- modules/internal/sg/main.tf
+++ modules/internal/sg/main.tf
@@ -2,27 +2,6 @@ resource "aws_security_group" "alb" {
   name        = "${terraform.workspace}-alb-sg"
   description = "Controls access to the ALB"
   vpc_id      = var.vpc_id
-
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # ingress {
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # egress {
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "alb_ingress" {
@@ -31,9 +10,6 @@ resource "aws_vpc_security_group_ingress_rule" "alb_ingress" {
   from_port = 80
   cidr_ipv4 = "0.0.0.0/0"
   security_group_id            = aws_security_group.alb.id
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_security_group" "app" {
